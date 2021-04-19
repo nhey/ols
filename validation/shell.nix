9012b3d87044968aae2c56d3cdfbe7cb859f3729
Nikolaj Hey Hinnerskov <nhey@oehle.dk>
2021-04-19 11:52:43 +0200

    Bump Futhark to latest master with machine epsilons.

--- validation/shell.nix
+++ validation/shell.nix
@@ -3,10 +3,10 @@ let
     "https://github.com/NixOS/nixpkgs/archive/df8e3bd110921621d175fad88c9e67909b7cb3d3.tar.gz"
   ) {};
 
-  futhark-version = "v0.19.3";
+  futhark-rev = "a29f2f580f02bd9ac43201cf9a9eb5c6f1e6347b";
   futhark-src = pkgs.applyPatches {
     name = "futhark-patched";
-    src = (fetchTarball "https://github.com/diku-dk/futhark/archive/${futhark-version}.tar.gz");
+    src = (fetchTarball "https://github.com/diku-dk/futhark/archive/${futhark-rev}.tar.gz");
     patches = [ ./futhark.patch ];
   };
   futhark-pinned = pkgs.haskellPackages.callPackage futhark-src { suffix = "nightly"; };
